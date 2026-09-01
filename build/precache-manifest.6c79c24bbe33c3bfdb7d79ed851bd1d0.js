535b8b5ac590826d00f6f390e8778aa757ab3508
Bryan Castro <bryan.castro@ppm.com.ec>
2026-09-01 17:29:42 -0300

    Add files via upload

---
+++ build/precache-manifest.6c79c24bbe33c3bfdb7d79ed851bd1d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad2fb95f6244fafa9b0e393eafd278e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70fc031d7f087b1079d9",
+    "url": "/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "24d7ddc2c4780399e258",
+    "url": "/static/js/2.27d8e5b0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.27d8e5b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70fc031d7f087b1079d9",
+    "url": "/static/js/main.36c2a1b6.chunk.js"
+  },
+  {
+    "revision": "03cc00eb0c215cdb696d",
+    "url": "/static/js/runtime-main.ff02b437.js"
+  }
+]);
